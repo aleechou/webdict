73da8587cbc0cd36e051c8830112b2e6ff457018
alee chou <zq@alee2002.com>
2012-12-02 16:05:49 +0800

    add same features of dict.cn on dict.qq cloud

--- dict.qq.js
+++ dict.qq.js
@@ -11,7 +11,50 @@
     } ;
     
     loadScript('http://dict.qq.com/cloudgetjs',function(){
-        
+           
+        var titleBar = document.getElementById("idQQCloudDictPopupToolbar") ;
+        if(titleBar)
+        {
+            var lnk = document.createElement("a") ;
+            lnk.innerText = "生词本" ;
+            lnk.setAttribute('style',"float: right;margin-right:30px") ;
+            lnk.setAttribute('href',"http://scb.dict.cn/") ;
+            lnk.setAttribute('target',"_blank") ;
+            titleBar.appendChild(lnk) ;
+            
+            // form of save to dict.cn
+            var form = document.createElement("form") ;
+            form.id = "__dictcn_form"
+            form.innerHTML = '<input id="___dictcn_newword" name="newword" /><input name="wordclass" value="0" />' ;
+            form.setAttribute('action',"http://scb.dict.cn/index.php?do=AddWord") ;
+            form.setAttribute('method',"post") ;
+            form.setAttribute('style',"display:none") ;
+            form.setAttribute('target',"_blank") ;
+            titleBar.appendChild(form) ;
+            
+            // save to dict.cn
+            lnk = document.createElement("a") ;
+            lnk.innerText = "[+]" ;
+            lnk.setAttribute('style',"float: right;margin-right:10px") ;
+            lnk.onclick = function(){
+                var input = document.getElementById("idQQCloudDictQueryInput") ;
+                
+                document.getElementById("___dictcn_newword").value = input.value ;
+                document.getElementById("__dictcn_form").submit() ;
+            }
+            titleBar.appendChild(lnk) ;
+            
+            // open dict.cn
+            lnk = document.createElement("a") ;
+            lnk.innerText = "Dict.cn" ;
+            lnk.setAttribute('style',"float: right;margin-right:30px") ;
+            lnk.setAttribute('target',"_blank") ;
+            lnk.onclick = function(){
+                var input = document.getElementById("idQQCloudDictQueryInput") ;
+                this.href = "http://dict.cn/" + input.value ;
+            }
+            titleBar.appendChild(lnk) ;
+        }
         
         document.onselectionchange = function(){
             var sel = window.getSelection() ;
@@ -34,7 +77,7 @@
             }
             
             
-            // http://scb.dict.cn/index.php?do=AddWord 
+            // http://scb.dict.cn/index.php?do=AddWord&wordclass=0&newword=hi
             // wordclass:0
             // newword:hi
             
